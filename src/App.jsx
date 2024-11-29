c35ad71bb989004a967058876301af1b0eab219c
parvez <parvezq033@gmail.com>
2024-11-29 17:23:57 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FigmApp from "./FigmApp";
+import "./App.css";
+
+export default function App() {
+
+  return (
+    <div>
+      <FigmApp />
+    </div>
+  )
+}
---
+++ src/FigmApp.jsx
@@ -0,0 +1,238 @@
+
+
+export default function FigmApp() {
+
+
+    return (
+
+        <div className="basebody">
+
+            <section className="firstsect">
+                <div className="mainheader">
+                    <div className="Upperheader">
+
+                        <img className="ui" src="./public\Logo.jpg" />
+
+                        <p>Home</p>
+
+                        <select className="dropdown1">
+                            <option>Product</option>
+                        </select>
+
+                        <select className="dropdown1">
+                            <option>Resources</option>
+                        </select>
+
+                        <p>Pricing</p>
+
+
+                    </div>
+                    <div className="profilepic1">
+                        <div className="secondpich">
+                            <img className="profilepic2" src="./public\Dropdown.jpg" />
+                        </div>
+                    </div>
+
+                </div>
+
+                <section className="intermidsec">
+
+                    <div className="intermiddiv">
+
+                        <div className="btnsect1">
+                            <p className="btnsect2">New features    </p>
+
+                            <p>checkout the team dashboard </p>
+
+
+                        </div>
+
+                        <div className="firstimd">
+                            <p>Beautiful analytics to grow smarter </p>
+                        </div>
+
+                        <div className="secondimd">Powerful self-serve product and growth
+                            analytics tp help you convert, engage and retain
+                            more users. trursted by over 4,000 startups </div>
+
+                        <div className="btndiv">
+                            <button className="demobtn">Demo</button>
+                            <button className="signbtn">Signup</button>
+                        </div>
+                    </div>
+
+
+                </section>
+                <section className="lastfsect">
+                    <img className="statimg" src="./public\Container.jpg" />
+                </section>
+
+
+
+
+
+                <div className="logosec">
+                    <div className="spntxt"><span > join 4,000+ companies already growing</span></div>
+
+                    <div className="logodiv">
+
+
+
+                        <img className="logoimg" src="./public\Fictional company logo (1).jpg" />
+                        <img className="logoimg" src="./public\Fictional company logo (2).jpg" />
+                        <img className="logoimg" src=" ./public\Fictional company logo.jpg" />
+                        <img className="logoimg" src=" ./public\Fictional company logo (3).jpg" />
+                        <img className="logoimg" src="./public\Fictional company logo (4).jpg" />
+                        <img className="logoimg" src="./public\Fictional company logo (5).jpg" />
+
+                    </div>
+
+                </div>
+
+                <section className="thirdsec">
+
+                    <div className="txtdiv">
+                        <div className="ftr0"><p className="ftr">features</p></div>
+                        <p>Analytics that feels like it from future</p>
+                    </div>
+
+                    <div className="txtdiv2">
+                        <p>Powerful self-serve Porduct and Growth analytics to hewlp you convert, engage, and reatin more users.
+                            Trusted by over 4,000 startups</p>
+                    </div>
+
+                    <div className="griddiv1">
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon.jpg" />
+                            <h4> Share team inboxes  </h4>
+                            <p>Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.</p>
+                        </div>
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (1).jpg" />
+                            <h4>Deliver instant answers  </h4>
+                            <p>An all-in-one customer service platform that helps you balance everything your customers need to be happy.</p>
+                        </div>
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (2).jpg" />
+                            <h4> Manage your team with reports  </h4>
+                            <p>Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.</p>
+                        </div>
+
+                    </div>
+                    <div className="griddiv2">
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (3).jpg" />
+                            <h4> Connect with customers  </h4>
+                            <p>Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion.</p>
+                        </div>
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (4).jpg" />
+                            <h4> Connect the tools you already use</h4>
+                            <p>Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools.</p>
+                        </div>
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (5).jpg" />
+                            <h4> Our people make the difference</h4>
+                            <p>We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="sisyphus">
+                    <div className="sis"><p></p></div>
+                    <div className="sis2"><p>We’ve been using Untitled to kick start every new <br /> project and can’t imagine working without it.</p></div>
+                    <div className="sis3">
+                        <img src="./public\public2\Avatar.jpg" />
+                        <img src="./public\public2\Text and supporting text.png" />
+                    </div>
+                </section>
+
+
+                <section className="fourthsec">
+                    <div className="fourthsec1"><p className="fourthsec11">features</p></div>
+                    <div className="fourthsec2"><p> self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.</p></div>
+                    <div className="fourthsec3">
+                        <img src="./public\public2\Content.jpg" />
+
+                    </div>
+                </section>
+
+                <section className="prevgrid">
+                    <div className="griddiv1">
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon.jpg" />
+                            <h4> Share team inboxes  </h4>
+                            <p>Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.</p>
+                            <div className="fourthsec1"><p className="fourthsec11">learn more</p></div>
+                        </div>
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (1).jpg" />
+                            <h4>Deliver instant answers  </h4>
+                            <p>An all-in-one customer service platform that helps you balance everything your customers need to be happy.</p>
+                            <div className="fourthsec1"><p className="fourthsec11">learn more</p></div>
+                        </div>
+
+                        <div className="gridsd">
+                            <img src="./public\public2\Featured icon (2).jpg" />
+                            <h4> Manage your team with reports  </h4>
+                            <p>Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.</p>
+                            <div className="fourthsec1"><p className="fourthsec11">learn more</p></div>
+                        </div>
+
+                    </div>
+                </section>
+
+
+                <section className="frequ" >
+                    <div className="freqdiv">
+                        <div className="freqhd"> <p>Frequently asked questions</p></div>
+
+                        <div className="freqhd2"><p>Everything you need to know about the product and billing.</p></div>
+
+                        <div className="freqhd3">Is there a free trial available?</div>
+
+
+                        <div className="freqhd4">Can I change my plan later?</div>
+                        <div className="freqhd4">What is your cancellation policy?</div>
+                        <div className="freqhd4">Can other info be added to an invoice?</div>
+                        <div className="freqhd4">How does billing work?</div>
+                        <div className="freqhd4">How do I change my account email?</div>
+                    </div>
+
+
+                </section>
+
+
+
+
+                <section className="querysec">
+
+                    <div className="queryimg"><img src="./public\Avatar group.jpg" /></div>
+                    <div className="queryq"><p>Still have questions?</p></div>
+                    <div className="querya"><p>Can’t find the answer you’re looking for? Please chat to our friendly team.</p></div>
+                    <div className="querybtn2"><button className="querybtn">Get in touch</button></div>
+                </section>
+
+
+
+
+
+
+
+
+
+            </section>
+
+
+
+        </div >
+    )
+}
